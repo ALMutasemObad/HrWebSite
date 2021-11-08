18f8d9e517f110877807fe49bb9afb36ab700b3f
ALMutasemObad <92812271+ALMutasemObad@users.noreply.github.com>
2021-11-08 23:27:47 +0300

    addEmpPhp

--- navigationbar.php
+++ navigationbar.php
@@ -3,13 +3,15 @@
 <link rel="stylesheet" href="style/style.css">
 </head>
 <body>
-    <?php require_once "header.php ";?>
+    <?php
+    require_once "config.php ";
+    require_once "header.php ";?>
 
 
 <ul>
   <li style="text-align: center;">
       <img src="img/1.jpg" alt="Mutasem" style="width:50px ">
-      <a href="logout.php"> Log Out</a>
+      <a href="logout.php"><b><?php echo htmlspecialchars($_SESSION["username"]); ?></b> Log Out</a>
     </li> 
   <li><a class="active" href="welcome.php">Home</a></li>
   <li><a href="employees.php">Employees</a></li>
