daf10c026abc1de1c93254c7b994df0dc0b53cc7
ALMutasemObad <92812271+ALMutasemObad@users.noreply.github.com>
2021-11-08 23:31:47 +0300

    updateDB

--- db/blackgemhrdb.sql
+++ db/blackgemhrdb.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 08 نوفمبر 2021 الساعة 01:15
+-- Generation Time: 08 نوفمبر 2021 الساعة 21:31
 -- إصدار الخادم: 10.4.20-MariaDB
 -- PHP Version: 8.0.8
 
@@ -31,16 +31,22 @@ CREATE TABLE `employee` (
   `id` int(10) NOT NULL,
   `firstname` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
   `lastname` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
-  `salary` float NOT NULL
+  `Role` text COLLATE utf8_unicode_ci NOT NULL,
+  `salary` float NOT NULL,
+  `JoinDate` date DEFAULT NULL,
+  `Image` blob DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 --
 -- إرجاع أو استيراد بيانات الجدول `employee`
 --
 
-INSERT INTO `employee` (`id`, `firstname`, `lastname`, `salary`) VALUES
-(1, 'motasm', 'abbad', 100000),
-(2, 'amr', 'atef', 134000);
+INSERT INTO `employee` (`id`, `firstname`, `lastname`, `Role`, `salary`, `JoinDate`, `Image`) VALUES
+(1, 'motasm', 'abbad', '', 100000, NULL, NULL),
+(2, 'amr', 'atef', '', 134000, NULL, NULL),
+(3, 'motasmw', 'OBAD', '', 0, '0000-00-00', NULL),
+(4, 'sfsdf', 'wqrwer', '', 0, '0000-00-00', NULL),
+(5, 'kahled', 'mohemmed', 'admin', 23424, '2021-10-30', NULL);
 
 -- --------------------------------------------------------
 
@@ -62,7 +68,8 @@ CREATE TABLE `users` (
 INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
 (1, 'mutasem', '$2y$10$43VAI6Suu.s02P/cN21WbeZLnTwZdJiYm3zW9DRIQKc3Is1Rg7yc6', '2021-11-07 22:25:58'),
 (2, 'blackgem', '$2y$10$KVorKHD.Z3F5xo7g7jFTKOuAQDIF1mTpp4Um1zjLCDveVy7E1MUgO', '2021-11-07 22:33:16'),
-(3, 'aaaa', '$2y$10$8VuTQnZ0ROGV2kda7ivbJuK2tyzNKa2rvJ0fqUqSmW2n/cq8lY85e', '2021-11-07 23:03:03');
+(3, 'aaaa', '$2y$10$8VuTQnZ0ROGV2kda7ivbJuK2tyzNKa2rvJ0fqUqSmW2n/cq8lY85e', '2021-11-07 23:03:03'),
+(4, 'm', '$2y$10$Zyy4Fa2V5B7wMMijnO/2.ORs2sgRnmcU9lkaOBaCY8wiXO6IFRmka', '2021-11-08 20:55:00');
 
 --
 -- Indexes for dumped tables
@@ -89,13 +96,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
